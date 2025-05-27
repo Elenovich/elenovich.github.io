fbe573a6f68424b64817b56a8c477f062cc720e2
Elenovich <lisemordov@gmail.com>
2025-05-27 13:46:15 +0400

    Update script.js

--- script.js
+++ script.js
@@ -2,16 +2,6 @@ document.getElementById("left-scroll-zone").addEventListener("click", function (
   window.scrollTo({ top: 0, behavior: "smooth" });
 });
 
-window.addEventListener("scroll", function () {
-  const scrollZone = document.getElementById("left-scroll-zone");
-  if (window.scrollY > 300) {
-    scrollZone.style.opacity = "1";
-    scrollZone.style.pointerEvents = "auto";
-  } else {
-    scrollZone.style.opacity = "0";
-    scrollZone.style.pointerEvents = "none";
-  }
-});
 window.addEventListener('scroll', function () {
   const navButtons = document.querySelector('.nav-buttons');
   const scrollTop = window.scrollY || window.pageYOffset;
